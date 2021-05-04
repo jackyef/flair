9c508f1c356d9f8387d350188e604ed1617a7ee8
Jacky Efendi <jacky.efendi1@gmail.com>
2021-05-04 21:24:24 +0700

    feat: Initial setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,23 @@
-import Link from 'next/link'
-
 export default function Home() {
   return (
-    <div>
-      Hello World.{' '}
-      <ul>
-        <li>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/ssr">
-            <a>SSR</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/ssg">
-            <a>SSG</a>
-          </Link>
-        </li>
-      </ul>
-    </div>
-  )
+    <>
+      <main>
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <h4>Heading 4</h4>
+        <h5>Heading 5</h5>
+        <h6>Heading 6</h6>
+        Hello world!
+      </main>
+      <style jsx>{`
+        main {
+          max-width: 1000px;
+          width: 100%;
+          margin: 0 auto;
+          padding: 16px;
+        }
+      `}</style>
+    </>
+  );
 }
