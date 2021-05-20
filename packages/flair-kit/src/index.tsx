180e9e3cd607d0f543cdb5b6d0a0455327b9dad3
jackyef <jacky.efendi1@gmail.com>
2021-05-20 22:54:57 +0700

    feat: setup build script for flair-kit

---
+++ packages/flair-kit/src/index.tsx
@@ -0,0 +1,14 @@
+/* Components */
+export * from './components/Anchor';
+export * from './components/Button';
+export * from './components/GlobalStyles';
+export * from './components/NoFlashScript';
+export * from './components/Typography';
+
+/* Context */
+export * from './context/theme';
+
+/* Utils */
+export { toggleColorScheme } from './utils/toggleColorScheme';
+export { getColorMapping } from './utils/getColorMapping';
+export { generateCssVariables } from './utils/generateCssVariables';
