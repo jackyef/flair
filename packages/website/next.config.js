eb09f21786a8c346c670ae3255220c3f4492e0a8
jackyef <jacky.efendi1@gmail.com>
2021-06-07 16:23:34 +0700

    feat: Add service-worker

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -1,3 +1,35 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withOffline = require('next-offline');
+
+module.exports = withOffline({
   /* regular next.js config options here */
-};
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+
+    runtimeCaching: [
+      {
+        urlPattern: /.(png|jpg|jpeg|webp|svg)$/,
+        handler: 'CacheFirst',
+      },
+      {
+        urlPattern: /api/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'offlineCache',
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+    ],
+  },
+});
