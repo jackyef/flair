f6635ebd3058d52a77d6cd1e7a80a3a1155abe9c
Jacky Efendi <jacky.efendi1@gmail.com>
2021-06-08 22:41:16 +0700

    Update next.config.js

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -4,10 +4,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 const { flowRight } = require('./utils/flow.js');
-const isProd = process.env.NODE_ENV !== 'development';
+const isProd = !!process.env.AWS_REGION; // This variable only exist on vercel deployments
 
 const config = {
-  target: 'serverless',
   /* regular next.js config options here */
   workboxOpts: {
     swDest: 'static/service-worker.js',
