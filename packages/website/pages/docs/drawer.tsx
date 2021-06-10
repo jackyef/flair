5e3da1f3156f1fa459b1c7e77e22f80d0528fd09
jackyef <jacky.efendi1@gmail.com>
2021-06-10 22:05:33 +0700

    fix: typo

--- packages/website/pages/docs/drawer.tsx
+++ packages/website/pages/docs/drawer.tsx
@@ -120,7 +120,7 @@ export default function DrawerPage() {
             return (
               <div>
                 <Button onClick={openDrawer}>
-                  Open dialog
+                  Open drawer
                 </Button>
                 <Drawer 
                   isOpen={isOpen}
