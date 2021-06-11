b986803c1e8732927879ecbe7c795dd854e1c98b
jackyef <jacky.efendi1@gmail.com>
2021-06-11 14:41:56 +0700

    feat: Add forwardRef for Text and Box

--- packages/flair-kit/src/components/Box/Box.tsx
+++ packages/flair-kit/src/components/Box/Box.tsx
@@ -2,6 +2,7 @@ import { css } from 'goober';
 import cx from 'classnames';
 import { useTheme } from '../../context/theme';
 import { SpaceVariant } from '../../theme/space';
+import { forwardRef } from 'react';
 
 type SpaceVariantWithZero = SpaceVariant | '0';
 type Space = SpaceVariantWithZero | SpaceVariantWithZero[];
@@ -12,41 +13,41 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
   padding?: Space;
 }
 
-export const Box: React.FC<Props> = ({
-  radii: pickedRadii,
-  margin,
-  padding,
-  className,
-  ...props
-}) => {
-  const { space, radii } = useTheme();
-  const getSpace = (spaceVariant?: Space) => {
-    if (Array.isArray(spaceVariant)) {
-      return spaceVariant.map((p) => space[p] || p).join(' ');
-    }
+export const Box = forwardRef<HTMLDivElement, Props>(
+  ({ radii: pickedRadii, margin, padding, className, ...props }, ref) => {
+    const { space, radii } = useTheme();
+    const getSpace = (spaceVariant?: Space) => {
+      if (Array.isArray(spaceVariant)) {
+        return spaceVariant.map((p) => space[p] || p).join(' ');
+      }
 
-    if (typeof spaceVariant === 'string') {
-      return space[spaceVariant];
-    }
+      if (typeof spaceVariant === 'string') {
+        return space[spaceVariant];
+      }
 
-    return '0';
-  };
-  const getRadii = () => {
-    if (Array.isArray(pickedRadii)) {
-      return pickedRadii.map((p) => radii[p] || p).join(' ');
-    }
+      return '0';
+    };
+    const getRadii = () => {
+      if (Array.isArray(pickedRadii)) {
+        return pickedRadii.map((p) => radii[p] || p).join(' ');
+      }
 
-    if (typeof pickedRadii === 'string') {
-      return radii[pickedRadii];
-    }
+      if (typeof pickedRadii === 'string') {
+        return radii[pickedRadii];
+      }
 
-    return '0';
-  };
-  const baseClass = css`
-    padding: ${getSpace(padding)};
-    margin: ${getSpace(margin)};
-    border-radius: ${getRadii()};
-  `;
+      return '0';
+    };
+    const baseClass = css`
+      padding: ${getSpace(padding)};
+      margin: ${getSpace(margin)};
+      border-radius: ${getRadii()};
+    `;
 
-  return <div className={cx(baseClass, className)} {...props} />;
-};
+    return <div ref={ref} className={cx(baseClass, className)} {...props} />;
+  }
+);
+
+if (process.env.NODE_ENV === 'development') {
+  Box.displayName = 'Box';
+}
--- packages/flair-kit/src/components/Text/Text.tsx
+++ packages/flair-kit/src/components/Text/Text.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react';
 import { css } from 'goober';
 import cx from 'classnames';
 import { useTheme } from '../../context/theme';
@@ -8,26 +9,22 @@ interface Props extends React.HTMLAttributes<HTMLSpanElement> {
   gradient?: MappedColorVariant[];
 }
 
-export const Text: React.FC<Props> = ({
-  variant = 'foreground',
-  gradient = [],
-  className,
-  ...props
-}) => {
-  const { colors } = useTheme();
-  const getGradient = () => {
-    if (gradient.length === 1) {
-      const c = gradient[0];
-      return `linear-gradient(90deg, ${colors[c][700].color}, ${colors[c][800].color})`;
-    }
+export const Text = forwardRef<HTMLSpanElement, Props>(
+  ({ variant = 'foreground', gradient = [], className, ...props }, ref) => {
+    const { colors } = useTheme();
+    const getGradient = () => {
+      if (gradient.length === 1) {
+        const c = gradient[0];
+        return `linear-gradient(90deg, ${colors[c][700].color}, ${colors[c][800].color})`;
+      }
 
-    return `linear-gradient(90deg, ${gradient
-      .map((c) => colors[c][700].color)
-      .join(',')});`;
-  };
-  const baseClass = css`
-    ${gradient.length > 0
-      ? `
+      return `linear-gradient(90deg, ${gradient
+        .map((c) => colors[c][700].color)
+        .join(',')});`;
+    };
+    const baseClass = css`
+      ${gradient.length > 0
+        ? `
           background-image: ${getGradient()};
           background-size: 100%;
           background-clip: text;
@@ -37,8 +34,13 @@ export const Text: React.FC<Props> = ({
           -moz-text-fill-color: transparent;
           -webkit-box-decoration-break: clone;
         `
-      : `color: ${colors[variant][500].color};`}
-  `;
+        : `color: ${colors[variant][500].color};`}
+    `;
 
-  return <span className={cx(baseClass, className)} {...props} />;
-};
+    return <span ref={ref} className={cx(baseClass, className)} {...props} />;
+  }
+);
+
+if (process.env.NODE_ENV === 'development') {
+  Text.displayName = 'Text';
+}
