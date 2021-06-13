c3c59da953962852b5cae1d104d74cd4937d3e98
jackyef <jacky.efendi1@gmail.com>
2021-06-13 17:43:03 +0700

    docs: Improve landing page

---
+++ packages/website/components/CommonAnchors/RepoAnchor.tsx
@@ -0,0 +1,5 @@
+import { Anchor } from 'flair-kit';
+
+export const RepoAnchor: React.FC = ({ children }) => {
+  return <Anchor href="https://github.com/jackyef/flair">{children}</Anchor>;
+};
--- packages/website/pages/index.tsx
+++ packages/website/pages/index.tsx
@@ -1,5 +1,5 @@
 import { css } from 'goober';
-import { useTheme, Box, H1, H3, P, Text, Button, Small } from 'flair-kit';
+import { useTheme, Box, H1, H3, P, Text, Button, Small, Code } from 'flair-kit';
 
 import Link from 'next/link';
 import { ArrowRightIcon } from '@heroicons/react/solid';
@@ -10,10 +10,11 @@ import { GooberAnchor } from '@/components/CommonAnchors/Goober';
 import { ReachUIAnchor } from '@/components/CommonAnchors/ReachUI';
 import { HeadlessUIAnchor } from '@/components/CommonAnchors/HeadlessUI';
 import { NextJSAnchor } from '@/components/CommonAnchors/NextJS';
+import { RepoAnchor } from '@/components/CommonAnchors/RepoAnchor';
 
 export default function Home() {
   const { space, mediaQuery } = useTheme();
-  const sectionCss = css`
+  const heroSectionCss = css`
     position: relative;
     display: flex;
     flex-direction: column;
@@ -25,6 +26,17 @@ export default function Home() {
 
     ${mediaQuery.onTabletUp} {
       flex-direction: row;
+    }
+  `;
+
+  const sectionCss = css`
+    max-width: 1000px;
+    width: 100%;
+    margin: 0 auto;
+    padding: ${space['lg']};
+
+    ${mediaQuery.onMobileUp} {
+      padding: ${space['3xl']};
     }
   `;
 
@@ -47,13 +59,17 @@ export default function Home() {
   `;
 
   const featureCss = css`
-    text-align: center;
+    text-align: left;
+
+    ${mediaQuery.onMobileUp} {
+      text-align: center;
+    }
   `;
 
   return (
     <div>
       <PageMetaTags />
-      <section className={sectionCss}>
+      <section className={heroSectionCss}>
         <HeroBackground />
         <div
           className={css`
@@ -80,10 +96,6 @@ export default function Home() {
             <Text gradient={['success']}>accessible</Text> React components.
           </H3>
 
-          <Small as="p">
-            (Initially written as exercise. Well, you could use it too, I guess)
-          </Small>
-
           <Box margin={['2xl', '0']}>
             <Link href="/docs/getting-started" passHref>
               <Button
@@ -101,36 +113,69 @@ export default function Home() {
         </div>
       </section>
 
-      <section className={featureGridCss}>
-        <div className={featureCss}>
-          <H3>Accessible</H3>
+      <section className={sectionCss}>
+        <H1 as="h2">Should you use this?</H1>
+        <div>
+          <H3 as="p">
+            Absolutely! <Small>(not)</Small>
+          </H3>
           <P>
-            Flair components are built with accessibility in mind. All
-            components are written to be accessible, leveraging some popular
-            third-party libraries like <ReachUIAnchor /> and{' '}
-            <HeadlessUIAnchor />.{' '}
+            Flair was initially written as a personal exercise. It is an attempt
+            to learn more about design, UX and accessibility side of the web.
+            The existing components works, but it is by no means, complete. Some
+            components that are quite commonly used such as <Code>Input</Code>,{' '}
+            <Code>CheckBox</Code>, <Code>RadioButton</Code>, etc. are not
+            implemented yet.
+          </P>
+
+          <P>
+            With that being said though, you are free to use this on
+            non-critical projects. Feel free to open issues on the{' '}
+            <RepoAnchor>GitHub repository</RepoAnchor> to report bugs or request
+            new components!
           </P>
         </div>
+      </section>
 
-        <div className={featureCss}>
-          <H3>Lightweight</H3>
-          <P>
-            Under the hood, Flair is powered by <GooberAnchor />, a 1 KB
-            CSS-in-JS library with easy-to-use APIs.
-          </P>
+      <section className={sectionCss}>
+        <div
+          className={css`
+            text-align: center;
+          `}
+        >
+          <H1 as="h2">Features</H1>
         </div>
+        <div className={featureGridCss}>
+          <div className={featureCss}>
+            <H3>Accessible</H3>
+            <P>
+              Flair components are built with accessibility in mind. All
+              components are written to be accessible, leveraging some popular
+              third-party libraries like <ReachUIAnchor /> and{' '}
+              <HeadlessUIAnchor />.{' '}
+            </P>
+          </div>
 
-        <div className={featureCss}>
-          <H3>Dark mode</H3>
-          <P>Who doesn&apos;t love some dark mode?</P>
-        </div>
+          <div className={featureCss}>
+            <H3>Lightweight</H3>
+            <P>
+              Under the hood, Flair is powered by <GooberAnchor />, a 1 KB
+              CSS-in-JS library with easy-to-use APIs.
+            </P>
+          </div>
 
-        <div className={featureCss}>
-          <H3>SSR ready</H3>
-          <P>
-            Flair is compatible with server-rendering. It works well with static
-            rendering framework like <NextJSAnchor />.
-          </P>
+          <div className={featureCss}>
+            <H3>Dark mode</H3>
+            <P>Who doesn&apos;t love some dark mode?</P>
+          </div>
+
+          <div className={featureCss}>
+            <H3>SSR ready</H3>
+            <P>
+              Flair is compatible with server-rendering. It works well with
+              static rendering framework like <NextJSAnchor />.
+            </P>
+          </div>
         </div>
       </section>
     </div>
