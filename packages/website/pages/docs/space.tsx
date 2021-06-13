e41aabe8932c1cae1ce7fbc4b63f2a25b51385a5
jackyef <jacky.efendi1@gmail.com>
2021-06-13 15:30:05 +0700

    docs: Add some words for space page

--- packages/website/pages/docs/space.tsx
+++ packages/website/pages/docs/space.tsx
@@ -1,5 +1,5 @@
 import { SpaceSquares } from '@/components/SpaceSquares/SpaceSquares';
-import { H1 } from 'flair-kit';
+import { Code, H1, P } from 'flair-kit';
 import { Main } from '@/components/Main/Main';
 
 export default function Home() {
@@ -7,6 +7,10 @@ export default function Home() {
     <Main>
       <H1>Space</H1>
 
+      <P>
+        This page shows the various <Code>space</Code> tokens exposed via the{' '}
+        <Code>useTheme()</Code> hook.
+      </P>
       <SpaceSquares />
     </Main>
   );
