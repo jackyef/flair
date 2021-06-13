f21646c9cf5a0adb3c477b7a639224ce2b786559
jackyef <jacky.efendi1@gmail.com>
2021-06-13 19:14:23 +0700

    feat: Add example nextjs project

---
+++ examples/nextjs/pages/index.js
@@ -0,0 +1,23 @@
+import { H1, P, Text, useTheme } from 'flair-kit';
+import { css } from 'goober';
+
+export default function HomePage() {
+  const { space } = useTheme();
+  return (
+    <div
+      className={css`
+        padding: 25vh ${space.xl};
+        margin: 0 auto;
+        max-width: 900px;
+        text-align: center;
+      `}
+    >
+      <H1>
+        <Text gradient={['dark']}>Next</Text> +{' '}
+        <Text gradient={['primary', 'secondary']}>Flair</Text>
+      </H1>
+
+      <P>Hello world! This is a sample Next.js app with Flair setup.</P>
+    </div>
+  );
+}
